27efb6552e8c30c5f59f1e6edf39c835589f7788
Marco Plaisier <mplaisier@bol.com>
2019-02-10 14:15:52 +0100

    #3 Refactor + simple tests

--- app.py
+++ app.py
@@ -1,11 +1,9 @@
-from datetime import datetime
 import os
+from datetime import datetime
 
 from flask import Flask, render_template, flash
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
-
-from forms import TextForm, MAX_TEXT_LENGTH
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = f"sqlite:///{os.getcwd()}/quotepad.db"
@@ -13,10 +11,18 @@ app.config['SECRET_KEY'] = "Ng6R83e9JvOV35Bc"
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
+from forms import TextForm
+from models import Text
+
 
 @app.route('/')
 def homepage():
     return render_template("homepage.html")
+
+
+def store(object):
+    db.session.add(object)
+    db.session.commit()
 
 
 @app.route('/text', methods=['GET', 'POST'])
@@ -24,10 +30,8 @@ def text():
     form = TextForm()
     if form.is_submitted():
         if form.validate_on_submit():
-            new_text = Text(text=form.ticker_text.data, active=form.active.data, created=datetime.now())
-            db.session.add(new_text)
-            db.session.commit()
-
+            new_text = Text.create_text_from_form(form)
+            store(new_text)
             flash("Saved")
         else:
             for _field, error_messages in form.errors.items():
@@ -38,14 +42,8 @@ def text():
 
 @app.route('/index')
 def index():
-    return render_template("index.html")
-
-
-class Text(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    text = db.Column(db.String(MAX_TEXT_LENGTH))
-    active = db.Column(db.Boolean)
-    created = db.Column(db.DateTime)
+    all_texts = Text.query.filter_by(active=True).order_by(Text.created).all()
+    return render_template("index.html", texts=all_texts)
 
 
 if __name__ == '__main__':
--- models.py
+++ models.py
@@ -1,4 +1,14 @@
+from datetime import datetime
 from app import db
 from forms import MAX_TEXT_LENGTH
 
 
+class Text(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    text = db.Column(db.String(MAX_TEXT_LENGTH))
+    active = db.Column(db.Boolean)
+    created = db.Column(db.DateTime)
+
+    @staticmethod
+    def create_text_from_form(form):
+        return Text(text=form.ticker_text.data, active=form.active.data, created=datetime.now())
