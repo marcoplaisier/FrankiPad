fe42e282bf37329bcdf36308cb4a2a778288db92
Marco Plaisier <mplaisier@bol.com>
2019-02-10 10:29:33 +0100

    #3 Create templates and simple flow

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, flash
+
+from forms import TextForm
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = "Ng6R83e9JvOV35Bc"
 
 
 @app.route('/')
@@ -8,9 +11,19 @@ def homepage():
     return render_template("homepage.html")
 
 
-@app.route('/create')
-def create():
-    return render_template("create.html")
+@app.route('/text', methods=['GET', 'POST'])
+def text():
+
+    form = TextForm()
+    if form.is_submitted():
+        if form.validate_on_submit():
+            flash("Saved")
+            print(form.ticker_text.data)
+        else:
+            for _field, error_messages in form.errors.items():
+                for error_message in error_messages:
+                    flash(error_message)
+    return render_template("text.html", form=form)
 
 
 @app.route('/index')
